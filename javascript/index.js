4a29677df6e67cf2925f8047f449b3e95af28ae5
Joel L <joelmleifer@gmail.com>
2018-09-19 22:44:25 +1000

    began gametick

--- javascript/index.js
+++ javascript/index.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     generateGameArea();
     gameData = generateGameData();
     generateTetrominoe();
+    gameTick();
 });
 
 var gameTick = function() {
@@ -13,8 +14,11 @@ var gameTick = function() {
 }
 
 var gameUpdate = function() {
-  if (Data.now() - gameData.lastTick > 250) {
-
+  if (Date.now() - gameData.lastTick > 250) {
+    gameData.lastTick = Date.now();
+    if (gameData.activeTetrominoe) {
+      moveTetrominoe();
+    }
   }
 };
 
@@ -29,12 +33,51 @@ var generateGameArea = function() {
 }
 
 var moveTetrominoe = function() {
-  
+  var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t'];
+  for (let i = 0; i < gameData.activeTiles.length; i++) {
+    console.log(gameData.activeTiles[i].Y, alphabet[i+1])
+    if (gameData.activeTiles[i].Y === gameData.staticTiles[findStaticTiles(alphabet[findFirstIndexFromArray(alphabet,gameData.activeTiles[i].Y)+1])].Y || gameData.activeTiles[i].Y === 't'){
+      for (let u = 0; u < gameData.activeTiles.length; i++) {
+        $('#' + String(gameData.activeTiles[i].X) + String(gameData.activeTiles[i].Y)).addClass('static');
+        $('#' + String(gameData.activeTiles[i].X) + String(gameData.activeTiles[i].Y)).removeClass('active');
+      }
+        gameData.activeTiles = [{Y:null,X:null},{Y:null,X:null},{Y:null,X:null},{Y:null,X:null}];
+        gameData.activeTetrominoe = false;
+        return;
+    }
+  }
+  for (let i = 0; i < gameData.activeTiles.length; i++) {
+    $('#' + String(gameData.activeTiles[i].X) + String(gameData.activeTiles[i].Y)).removeClass('tetrominoe-' + gameData.tetrominoeShapes[gameData.activeTetrominoeShape]);
+    $('#' + String(gameData.activeTiles[i].X) + String(gameData.activeTiles[i].Y)).removeClass('active');
+    console.log('removed classes');
+  }
+  for (let i = 0; i < gameData.activeTiles.length; i++) {
+    console.log('added classes');
+    gameData.activeTiles[i].Y = alphabet[findFirstIndexFromArray(alphabet,gameData.activeTiles[i].Y) + 1];
+    $('#' + String(gameData.activeTiles[i].X) + String(gameData.activeTiles[i].Y)).addClass('tetrominoe-' + gameData.tetrominoeShapes[gameData.activeTetrominoeShape]);
+    $('#' + String(gameData.activeTiles[i].X) + String(gameData.activeTiles[i].Y)).addClass('active');
+  }
+}
+
+var findFirstIndexFromArray = function(array, variable) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === variable) {
+      return i;
+    }
+  }
+}
+
+var findStaticTiles = function (Y) {
+  for (let i = 0; i < gameData.staticTiles.length; i++) {
+    if (Y === gameData.staticTiles[i].Y) {
+      return i;
+    }
+  }
 }
 
 var generateTetrominoe = function() {
-    var index = Math.floor(Math.random()*7)
-    var currentTetrominoe = gameData.tetrominoeConfigurations[index];
+    gameData.activeTetrominoeShape = Math.floor(Math.random()*7)
+    var currentTetrominoe = gameData.tetrominoeConfigurations[gameData.activeTetrominoeShape];
     var currentTile = currentTetrominoe[0]
     console.log(currentTetrominoe);
     for (let i = 0; i < 4; i++) {
@@ -48,9 +91,10 @@ var generateTetrominoe = function() {
         gameData.activeTiles[i].X = currentTile + 2;
       }
       console.log('#' + String(gameData.activeTiles[i].X) + String(gameData.activeTiles[i].Y))
-      $('#' + String(gameData.activeTiles[i].X) + String(gameData.activeTiles[i].Y)).addClass('tetrominoe-' + gameData.tetrominoeShapes[index]);
+      $('#' + String(gameData.activeTiles[i].X) + String(gameData.activeTiles[i].Y)).addClass('tetrominoe-' + gameData.tetrominoeShapes[gameData.activeTetrominoeShape]);
       $('#' + String(gameData.activeTiles[i].X) + String(gameData.activeTiles[i].Y)).addClass('active');
     }
+    gameData.activeTetrominoe = true;
 }
 
 generateGameData = function() {
@@ -62,8 +106,13 @@ generateGameData = function() {
     tetrominoeShapes: ['I','O','T','S','Z','J','L'],
     tetrominoeConfigurations: [['5','6','7','8'],['2','3','6','7'],['2','5','6','7'],['2','3','5','6'],['1','2','6','7'],['1','5','6','7'],['3','5','6','7']],
     tetrominoeColors: ['#2ae0ed','#fce93c','#b539ba','#38bc31','#e04d1d','#4773e0','#ed911a'],
+    activeTetrominoe: false,
+    activeTetrominoeShape: null,
     activeTiles: [{Y:null,X:null},{Y:null,X:null},{Y:null,X:null},{Y:null,X:null}],
-    staticTiles: staticTiles
+    staticTiles: staticTiles,
+
+
+    lastTick: 0
   }
 }
 
