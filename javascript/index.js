9ba943a0165d6d8e0bd51ddaf64d075026b26c3c
Joel L <joelmleifer@gmail.com>
2018-09-20 22:00:07 +1000

    added gameover framework

--- javascript/index.js
+++ javascript/index.js
@@ -8,8 +8,9 @@ $(document).ready(function() {
     $('html').keypress(function(key){
       switch (key.keyCode) {
         case 13: if (gameData.activeTetrominoe){return}else{generateTetrominoe()} break;
-        case 97: moveTetrominoe('left'); break;
-        case 100: moveTetrominoe('right'); break;
+        case 97: if(gameData.tickrate === gameData.dropRate){return}moveTetrominoe('left'); break;
+        case 100: if(gameData.tickrate === gameData.dropRate){return}moveTetrominoe('right'); break;
+        case 32: if (gameData.activeTetrominoe){gameData.tickrate = gameData.dropRate;}; break;
         default: console.log('invalid key');
       }
     })
@@ -19,10 +20,6 @@ var gameTick = function() {
   gameData.lastTick = Date.now();
   var tick = setInterval(function(){
     gameUpdate()
-    if(findStaticTiles('a')) {
-      $('#score').append('GAMEOVER');
-      clearInterval(tick)
-    }
   },1)
 }
 
@@ -32,10 +29,8 @@ var gameUpdate = function() {
     if (gameData.activeTetrominoe) {
       fallTetrominoe();
     }
-    
   }
 };
-
 
 var generateGameArea = function() {
   var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t'];
@@ -80,6 +75,13 @@ var moveTetrominoe = function(direction) {
 }
 
 var fallTetrominoe = function() {
+  for (let i = 0; i < gameData.activeTiles.length; i++) {
+    if (findStaticTiles(gameData.activeTiles[i].X,gameData.activeTiles[i].X)) {
+      console.log('found static')
+      gameOver();
+      return;
+    }
+  }
   var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t'];
   var ending = false;
   var currentTile = gameData.activeTiles[0];
@@ -103,6 +105,7 @@ var fallTetrominoe = function() {
     }
       gameData.activeTiles = [{Y:null,X:null},{Y:null,X:null},{Y:null,X:null},{Y:null,X:null}];
       gameData.activeTetrominoe = false;
+      gameData.tickrate = 250;
   }
   else {
     for (let i = 0; i < gameData.activeTiles.length; i++) {
@@ -117,12 +120,54 @@ var fallTetrominoe = function() {
   }
 }
 
+var rotateTetrominoe = function() {
+  switch (gameData.activeTetrominoeShape) {
+    case 'I': 
+      switch (gameData.activeTetrominoeRotation) {
+        case 0: 
+          if(!$('#'+gameData.activeTiles[1].X + findYValue(gameData.activeTiles[0].Y,false)).hasClass('static') && !$('#'+gameData.activeTiles[1].X + findYValue(gameData.activeTiles[0].Y,true)).hasClass('static') && !$('#'+gameData.activeTiles[1].X + findYValue(findYValue(gameData.activeTiles[0].Y,true),true)).hasClass('static')) {
+
+          }
+        break;
+        case 1:
+        case 2:
+        case 3:
+        default: console.log('Invalid rotation paramter');
+      }
+    break;
+    case 'O':
+
+    break;
+    case 'T': 
+
+    break;
+    case 'S': 
+
+    break;
+    case 'Z': 
+
+    break;
+    case 'J': 
+
+    break;
+    case 'L': 
+
+    break;
+    default: return;
+  }
+}
+
 var findFirstIndexFromArray = function(array, variable) {
   for (let i = 0; i < array.length; i++) {
     if (array[i] === variable) {
       return i;
     }
   }
+}
+
+var findYValue = function(Y,next) {
+  var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t'];
+
 }
 
 var findStaticTiles = function (X,Y) {
@@ -150,7 +195,16 @@ var addTileToStatic = function (tile) {
   }
 }
 
+var gameOver = function() {
+  gameData.gameOver = true;
+  $('.instructions').replace('<div class="instructions">GAMEOVER</div>');
+}
+
 var generateTetrominoe = function() {
+    if (gameData.gameOver) {
+      return;
+    }
+    gameData.activeTetrominoeRotation = 0;
     gameData.activeTetrominoeShape = Math.floor(Math.random()*7)
     var currentTetrominoe = gameData.tetrominoeConfigurations[gameData.activeTetrominoeShape];
     var currentTile = currentTetrominoe[0]
@@ -177,13 +231,16 @@ generateGameData = function() {
   }
   return {
     tetrominoeShapes: ['I','O','T','S','Z','J','L'],
-    tetrominoeConfigurations: [['5','6','7','8'],['2','3','6','7'],['2','5','6','7'],['2','3','5','6'],['1','2','6','7'],['1','5','6','7'],['3','5','6','7']],
+    tetrominoeConfigurations: [['1','2','3','4'],['2','3','6','7'],['2','5','6','7'],['2','3','5','6'],['1','2','6','7'],['1','5','6','7'],['3','5','6','7']],
     tetrominoeColors: ['#2ae0ed','#fce93c','#b539ba','#38bc31','#e04d1d','#4773e0','#ed911a'],
     activeTetrominoe: false,
     activeTetrominoeShape: null,
+    activeTetrominoeRotation: 0,
     activeTiles: [{Y:null,X:null},{Y:null,X:null},{Y:null,X:null},{Y:null,X:null}],
     staticTiles: staticTiles,
 
+    gameOver: false,
+    dropRate: 20,
     tickrate:250,
     lastTick: 0
   }
